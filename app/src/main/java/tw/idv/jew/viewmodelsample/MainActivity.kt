4fefd67c498304f36ecf798f0b1ac06f7c6efc45
a0916105 <whdofjew@gmail.com>
2021-04-24 11:39:52 +0800

    ViewModelProvider

--- app/src/main/java/tw/idv/jew/viewmodelsample/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/viewmodelsample/MainActivity.kt
@@ -2,10 +2,23 @@ package tw.idv.jew.viewmodelsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    lateinit var factory: UserListViewModel.Factory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //No factory
+        val viewModel = ViewModelProvider(this)
+            .get(UserListViewModel::class.java)
+        println(viewModel.userList)
+
+        //Factory
+        val viewModelWithFactory = ViewModelProvider(this, factory)
+            .get(UserListViewModel::class.java)
+        println(viewModelWithFactory.userList)
     }
 }
--- app/src/main/java/tw/idv/jew/viewmodelsample/UserListViewModel.kt
+++ app/src/main/java/tw/idv/jew/viewmodelsample/UserListViewModel.kt
@@ -1,7 +1,42 @@
 package tw.idv.jew.viewmodelsample
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
-class UserListViewModel : ViewModel() {
-    val userList = listOf("John", "Marry")
+class UserListViewModel(private val userRepository: UserRepository) : ViewModel() {
+    val userList = userRepository.getUserInfo()
+
+    class Factory(private val userRepository: UserRepository) : ViewModelProvider.Factory{
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return UserListViewModel(userRepository) as T
+        }
+
+    }
 }
+
+// User 倉庫
+class UserRepository {
+
+    fun getUserInfo(): MutableLiveData<User> {
+        val user = MutableLiveData<User>()
+        user.value = User("張三","18")
+        return user
+    }
+
+    companion object {
+
+        // For Singleton instantiation
+        @Volatile
+        private var instance: UserRepository? = null
+
+        fun getInstance() =
+            instance ?: synchronized(this) {
+                instance ?: UserRepository().also { instance = it }
+            }
+    }
+}
+
+// User 實體
+data class User(val name:String,val age:String)
